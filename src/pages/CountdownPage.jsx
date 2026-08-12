2f60de1481a71b216c7aa5da394972789e01312f
tjfgml8054@naver.com <tjfgml8054@naver.com>
2026-08-12 19:45:48 +0900

    Refactor: useCountdown으로 리팩토링

--- src/pages/CountdownPage.jsx
+++ src/pages/CountdownPage.jsx
@@ -1,32 +1,9 @@
 import { useState, useEffect } from "react";
 import { useGoBack } from "../hooks/useGoBack";
+import { useCountdown } from "../hooks/useCountdown";
 
 export const Countdown = () => {
-  const targetDate = new Date("2026-08-25T00:00:00");
-
-  const calculateTimeLeft = (targetDate) => {
-    const difference = targetDate.getTime() - new Date().getTime();
-
-    if (difference <= 0) {
-      return { days: 0, hours: 0, minutes: 0, seconds: 0 };
-    }
-
-    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((difference / (1000 * 60 * 60)) % 24);
-    const minutes = Math.floor((difference / (1000 * 60)) % 60);
-    const seconds = Math.floor((difference / 1000) % 60);
-
-    return { days, hours, minutes, seconds };
-  };
-  const [timeLeft, setTimeLeft] = useState(() => calculateTimeLeft(targetDate));
-
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setTimeLeft(calculateTimeLeft(targetDate));
-    }, 1000);
-
-    return () => clearInterval(timer);
-  }, [targetDate]);
+  const timeLeft = useCountdown();
 
   const goBack = useGoBack();
 
@@ -40,6 +17,7 @@ export const Countdown = () => {
       <p>{`${timeLeft.days}일 ${timeLeft.hours}시간 ${timeLeft.minutes}분 ${timeLeft.seconds}초`}</p>
     </div>
   );
+
 };
 
 export default Countdown;
