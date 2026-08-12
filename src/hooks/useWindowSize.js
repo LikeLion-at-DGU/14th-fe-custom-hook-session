c6269c50d4fe5873262893cbb6f54c227cb5e3e8
dogdevelophoyri <hyori7272@gmail.com>
2026-08-12 19:44:57 +0900

    Refactor: useWindowSize로 리팩토링

--- src/hooks/useWindowSize.js
+++ src/hooks/useWindowSize.js
@@ -1,1 +1,25 @@
-// 커스텀훅 코드를 작성해보세요!
+import { useEffect, useState } from "react";
+
+export const useWindowSize = () => {
+const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+    });
+
+    useEffect(() => {
+        const handleResize = () => {
+        setWindowSize({
+            width: window.innerWidth,
+            height: window.innerHeight,
+        });
+        };
+
+        window.addEventListener("resize", handleResize);
+
+        return () => {
+        window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+
+    return { windowSize };
+}
--- src/pages/WindowSizePage.jsx
+++ src/pages/WindowSizePage.jsx
@@ -1,27 +1,9 @@
-import { useEffect, useState } from "react";
 import { useGoBack } from "../hooks/useGoBack";
-
+import { useWindowSize } from "../hooks/useWindowSize"
 export const WindowSize = () => {
   // 실습 1. 하단 코드를 useWindowSize (커스텀 훅으로 바꿔주세요!)
-  const [windowSize, setWindowSize] = useState({
-    width: window.innerWidth,
-    height: window.innerHeight,
-  });
-
-  useEffect(() => {
-    const handleResize = () => {
-      setWindowSize({
-        width: window.innerWidth,
-        height: window.innerHeight,
-      });
-    };
-
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
+  
+  const { windowSize } = useWindowSize();
 
   const goBack = useGoBack();
   return (
