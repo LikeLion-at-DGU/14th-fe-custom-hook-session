332088d318b21bea69be2bcbeaad20481d6537ac
rhtjdco <gsc6690@gmail.com>
2026-08-12 20:29:14 +0900

    Feature: useMousePosition 구현

--- src/hooks/useSomething.js
+++ src/hooks/useSomething.js
@@ -1,7 +1,30 @@
-export const useSomething = () => {
+import { useState, useEffect } from "react";
+
+export const useMousePosition = () => {
   // 여러분의 use{Something}을 만들어주세요!
   // 정답은 없습니다. 커스텀훅의 필요성을 스스로 느껴보세요.
   // 아이디어를 생각하고, 스스로 구현하다가 어려우면 손 들어주세요!
+  const [mousePosition, setMousePosition] = useState({
+    x: 0,
+    y: 0,
+  });
+
+  useEffect(() => {
+    const myMouseMove = () => {
+      setMousePosition({
+        x: event.clientX,
+        y: event.clientY,
+      });
+    };
+
+    window.addEventListener("mousemove", myMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", myMouseMove);
+    };
+  }, []);
+
+  return mousePosition;
 };
 
 // export default useSomething;
--- src/pages/YourOwnHookPage.jsx
+++ src/pages/YourOwnHookPage.jsx
@@ -1,10 +1,10 @@
-import { useSomething } from "../hooks/useSomething";
+import { useMousePosition } from "../hooks/useSomething";
 import { useGoBack } from "../hooks/useGoBack";
 
 const YourOwnHook = () => {
   // const { something... } = useSomething();
   // 하단 UI에 자유롭게 위에서 받아온 값들을 바인딩 해보세요~
-
+  const mousePosition = useMousePosition();
   const goBack = useGoBack();
 
   return (
@@ -13,6 +13,8 @@ const YourOwnHook = () => {
         뒤로가기
       </button>
       <h2 className="main-title">useSomething 실습</h2>
+      <p>마우스 x 좌표: {mousePosition.x}</p>
+      <p>마우스 y 좌표: {mousePosition.y}</p>
     </div>
   );
 };
