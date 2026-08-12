282b2be4082bbaade6f4acbac4fec02ae9101cab
huisuu4 <dlgmltn4856@gmail.com>
2026-08-12 20:47:52 +0900

    Feature: useToast 구현

--- src/hooks/useSomething.js
+++ src/hooks/useSomething.js
@@ -1,7 +1,22 @@
+import { useState } from "react";
 export const useSomething = () => {
   // 여러분의 use{Something}을 만들어주세요!
   // 정답은 없습니다. 커스텀훅의 필요성을 스스로 느껴보세요.
   // 아이디어를 생각하고, 스스로 구현하다가 어려우면 손 들어주세요!
+
+    const [isVisible, setIsVisible] = useState(false);
+  const [message, setMessage] = useState("");
+
+  const showToast = (text) => {
+    setMessage(text);
+    setIsVisible(true);
+
+    setTimeout(() => {
+      setIsVisible(false);
+    }, 3000);
+  };
+
+  return { isVisible, message, showToast };
 };
 
 // export default useSomething;
--- src/pages/YourOwnHookPage.jsx
+++ src/pages/YourOwnHookPage.jsx
@@ -4,6 +4,7 @@ import { useGoBack } from "../hooks/useGoBack";
 const YourOwnHook = () => {
   // const { something... } = useSomething();
   // 하단 UI에 자유롭게 위에서 받아온 값들을 바인딩 해보세요~
+  const { isVisible, message, showToast } = useSomething();
 
   const goBack = useGoBack();
 
@@ -13,6 +14,35 @@ const YourOwnHook = () => {
         뒤로가기
       </button>
       <h2 className="main-title">useSomething 실습</h2>
+
+      <button 
+        onClick={() => showToast("토스트 버튼! 💾")}
+        style={{ 
+          padding: "10px", 
+          fontSize: "16px", 
+          color: "#ffffff",
+          backgroundColor: "#000000"
+        }}
+      >
+        토스트 버튼
+      </button>
+
+      {isVisible && (
+        <div 
+          style={{
+            position: "fixed",
+            bottom: "50px",
+            left: "50%",
+            transform: "translateX(-50%)",
+            backgroundColor: "#ffffff",
+            color: "#000000",
+            padding: "12px 24px",
+            borderRadius: "8px",
+          }}
+        >
+          {message}
+        </div>
+      )}
     </div>
   );
 };
