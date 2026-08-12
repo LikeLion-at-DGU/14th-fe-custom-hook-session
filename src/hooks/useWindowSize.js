bb6ad69b4357b0340f15e99f1b086c69826e626b
onlyvalue <sarahkim2917@gmail.com>
2026-08-12 19:46:07 +0900

    Refactor: useCountdown으로 리팩토링

--- src/hooks/useWindowSize.js
+++ src/hooks/useWindowSize.js
@@ -1,1 +1,24 @@
 // 커스텀훅 코드를 작성해보세요!
+import { useState, useEffect } from "react";
+
+export const useWindowSize = () => {
+    const [windowSize, setWindowSize] = useState({
+        width: 0,
+        height: 0,
+      });
+    
+    useEffect(() => {
+        const handleResize = () => {
+          setWindowSize({
+            width: window.innerWidth,
+            height: window.innerHeight,
+          });
+        };
+        window.addEventListener("resize", handleResize);
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+    return {windowSize};
+};
