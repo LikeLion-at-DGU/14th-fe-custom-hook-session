cfc240ed9b06384ca2326b48418f2b8beebb190e
hyunaway <a01043074762@gmail.com>
2026-08-14 07:49:18 -0400

    Refactor: useWindowSize로 리팩토링

--- src/hooks/useWindowSize.js
+++ src/hooks/useWindowSize.js
@@ -1,1 +1,25 @@
-// 커스텀훅 코드를 작성해보세요!
+import { useEffect, useState } from "react";
+
+export const useWindowSize = () => { // 커스텀 훅 정의 규칙 : use + 뒤 단어는 대문자로 시작
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return { windowSize };
+};
