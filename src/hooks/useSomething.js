032e0317ce0ab67fb3b5f2ac82c501fe077c140f
feseungwon <aza0909@dgu.ac.kr>
2026-08-16 22:43:28 +0900

    Feature: useSomething 구현

--- src/hooks/useSomething.js
+++ src/hooks/useSomething.js
@@ -1,8 +1,23 @@
-export const useSomething = () => {
+import { useState } from "react";
+
+export function useSomething() {
+  const [money, setMoney] = useState(0);
+  const [rate, setRate] = useState(0);
+
+  const start = () => {
+    setMoney(Number(prompt("초기 금액")));
+    setRate(Number(prompt("이율 (%)")));
+  };
+
+  const compound = () => {
+    setMoney((prev) => prev * (1 + rate / 100));
+  };
+
+  return { money, start, compound };
+}
   // 여러분의 use{Something}을 만들어주세요!
   // 정답은 없습니다. 커스텀훅의 필요성을 스스로 느껴보세요.
   // 아이디어를 생각하고, 스스로 구현하다가 어려우면 손 들어주세요!
-};
 
 // export default useSomething;
 
--- src/pages/YourOwnHookPage.jsx
+++ src/pages/YourOwnHookPage.jsx
@@ -4,6 +4,7 @@ import { useGoBack } from "../hooks/useGoBack";
 const YourOwnHook = () => {
   // const { something... } = useSomething();
   // 하단 UI에 자유롭게 위에서 받아온 값들을 바인딩 해보세요~
+  const { money, start, compound } = useSomething();
 
   const goBack = useGoBack();
 
@@ -13,6 +14,11 @@ const YourOwnHook = () => {
         뒤로가기
       </button>
       <h2 className="main-title">useSomething 실습</h2>
+    <button onClick={start}>금액 입력</button> 
+    <br/><br/>
+    <button onClick={compound}>복리 적용</button>
+    <br/><br/>
+    <p>{money}원</p>
     </div>
   );
 };
